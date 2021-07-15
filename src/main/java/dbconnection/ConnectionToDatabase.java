263d826090cc6cae4c107483aa858e541222b5e4
jundev <merdeliyev@inbox.ru>
2021-07-16 00:53:58 +0400

    Connection class was updated! I removed an unnecessary class "DatabaseConnectionParser" for Properties which read the url, user and password details from file. Instead, I use DriverManager.getConnection(String url, Properties info) method and created a Properties object inside ConnectionToDatabase class

--- src/main/java/dbconnection/ConnectionToDatabase.java
+++ src/main/java/dbconnection/ConnectionToDatabase.java
@@ -1,23 +1,26 @@
 package dbconnection;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
-public class ConnectionToDatabase{
+public class ConnectionToDatabase {
 
     private Connection connection;
 
-
     public Connection getConnection() {
-        DatabaseConnectionParser dbDetails = new DatabaseConnectionParser();
-        dbDetails.parseFromFile();
+        Properties properties = new Properties();
+        File file = new File("C:\\Users\\m.ulvi\\IdeaProjects\\myshop1\\src\\main\\webapp\\WEB-INF\\application.properties");
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            this.connection = DriverManager.getConnection(dbDetails.getUrl(),
-                    dbDetails.getUser(), dbDetails.getPassword());
-
-        } catch (ClassNotFoundException | SQLException e) {
+            FileReader fileReader = new FileReader(file);
+            properties.load(fileReader);
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mycustomshop", properties);
+        } catch (ClassNotFoundException | SQLException | IOException e) {
             e.printStackTrace();
         }
         return this.connection;
